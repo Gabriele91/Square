775a387d23841ec64f0d43f7b93d8935bf3d0b26
Gabriele91 <dbgabri@gmail.com>
2017-11-12 00:46:00 +0100

    Added "Data\Archive.h"

---
+++ include/Square/Data/Archive.h
@@ -0,0 +1,18 @@
+//
+//  Square
+//
+//  Created by Gabriele on 09/09/16.
+//  Copyright © 2016 Gabriele. All rights reserved.
+//
+#pragma once
+#include "Square/Config.h"
+#include "Square/Core/Uncopyable.h"
+#include "Square/Core/SmartPointers.h"
+
+namespace Square
+{
+namespace  Data
+{
+	//TODO
+}
+}
