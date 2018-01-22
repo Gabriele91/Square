a3f73cea738e140704a02456912d733532e1b7b1
Gabriele91 <dbgabri@gmail.com>
2018-01-22 17:05:50 +0100

    Added shader resource (WIP)

---
+++ source/Resource/Shader.cpp
@@ -0,0 +1,35 @@
+﻿//  Square
+//
+//  Created by Gabriele on 15/08/16.
+//  Copyright � 2016 Gabriele. All rights reserved.
+//
+#include "Square/Config.h"
+#include "Square/Core/Filesystem.h"
+#include "Square/Core/Context.h"
+#include "Square/Core/Attribute.h"
+#include "Square/Resource/Shader.h"
+#include "Square/Core/ClassObjectRegistration.h"
+
+namespace Square
+{
+namespace Resource
+{
+    //////////////////////////////////////////////////////////////
+    //Add element to objects
+    SQUARE_CLASS_OBJECT_REGISTRATION(Shader);
+    //Registration in context
+    void Shader::object_registration(Context& ctx)
+    {
+        //factory
+        ctx.add_resource<Shader>({ ".glsl", ".hlsl" });
+		//attributes
+		ctx.add_attributes<Shader>(attribute_function<Shader, void*>
+		("constant_buffer"
+		, (void*)(nullptr)
+		, [](const Shader* shader) -> void* { return (void*)shader->constant_buffer(name); }
+		, [](Shader* shader, const void*&) { /*none*/ }
+		));
+        //end
+    }
+}
+}
