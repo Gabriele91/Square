70d5ac4e92b31b6e6b26dfab00f2ac7bad1b94bd
Gabriele91 <dbgabri@gmail.com>
2018-07-26 18:18:38 +0200

    Vertex[2D/3D] -> Position[2D/3D] + m_uvmap -> m_uv

--- common/Vertex.hlsl
+++ common/Vertex.hlsl
@@ -6,47 +6,55 @@
 //  Copyright © 2018 Gabriele Di Bari. All rights reserved.
 //
 
-//base vertex (2D)
-struct Vertex2D
+//position (2D)
+struct Position2D
 {
     Vec2 m_position : POSITION;
 };
+#define Pos2D Position2D
 
-//base vertex (2D) + uv
-struct Vertex2DUV
+//position (2D) + uv
+struct Position2DUV
 {
     Vec2 m_position : POSITION;
     Vec2 m_uvmap    : TEXCOORD0;
 };
+#define Pos2DUV Position2DUV
 
-//base vertex (3D)
-struct Vertex3D
+//position (3D)
+struct Position3D
 {
     Vec3 m_position : POSITION;
 };
+#define Pos3D Position3D
 
-//base vertex (3D) + uv
-struct Vertex3DUV
+//position (3D) + uv
+struct Position3DUV
 {
     Vec3 m_position : POSITION;
-    Vec2 m_uvmap    : TEXCOORD0;
+    Vec2 m_uv       : TEXCOORD0;
 };
+#define Pos3DUV Position3DUV
 
-//vertex (3D) + normal + uv
-struct Vertex3DNormalUV
+//position (3D) + normal + uv
+struct Position3DNormalUV
 {
     Vec3 m_position : POSITION;
     Vec3 m_normal   : NORMAL0;
-    Vec2 m_uvmap    : TEXCOORD0;
+    Vec2 m_uv       : TEXCOORD0;
 };
+#define Pos3DNormalUV Position3DNormalUV
+#define Pos3DNorUV Position3DNormalUV
 
-//vertex (3D) + normal + tangent + bitangent + uv
-struct Vertex3DNormalUVTangetBinomial
+//position (3D) + normal + tangent + bitangent + uv
+struct Position3DNormalTangetBinomialUV
 {
     Vec3 m_position : POSITION;
     Vec3 m_normal   : NORMAL0;
     Vec3 m_tangent  : TANGENT0;
     Vec3 m_binomial : BINORMAL0;
-    Vec2 m_uvmap    : TEXCOORD0;
+    Vec2 m_uv       : TEXCOORD0;
 };
+#define Pos3DNormalTangetBinomialUV Position3DNormalTangetBinomialUV
+#define Pos3DNorTanBinUV Position3DNormalTangetBinomialUV
 
