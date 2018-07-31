76c5394e198b969c7883e09e13d24877233041cd
Gabriele91 <dbgabri@gmail.com>
2018-07-31 22:25:06 +0200

    Update example

--- example/HelloWorld/resources.rs
+++ example/HelloWorld/resources.rs
@@ -1,6 +1,6 @@
 resources
 {
-	path "../../common" filter "(\\w)+.hlsl" //Windows/Linux
-	path "common" filter "(\\w)+.hlsl" //macOS
+	path "../../common" filter "(\\w)+.rs" //Windows/Linux
+	path "common" filter "(\\w)+.rs" //macOS
 	path "assets"
 }
