5925be6ec9d4f74f0d178adabc01d5818df3c60b
Gabriele91 <dbgabri@gmail.com>
2018-07-31 20:25:13 +0200

    WIP lights

--- common/DirectionLight.hlsl
+++ common/DirectionLight.hlsl
@@ -6,25 +6,36 @@
 //  Copyright © 2018 Gabriele Di Bari. All rights reserved.
 //
 #pragma once
-
 struct DirectionLight
 {
     Vec3 m_diffuse;
     Vec3 m_specular;
 };
 
-Vec3 light_compute_diffuse
-(
-     in Vec4  fposition,
-     in Vec3  vdir,
-     in Vec3  normal,
-     in float shininess
-);
+cbuffer Light
+{
+    DirectionLight light;
+}
 
-Vec3 light_compute_specular
+LightResult compute_light
 (
-     in Vec4  fposition,
+     in Vec3  fposition,
      in Vec3  vdir,
      in Vec3  normal,
      in float shininess
-);
+)
+{
+    // Light dir
+    Vec3 light_dir = normalize(light.m_direction);
+    // Diffuse shading
+    float diff = max(dot(normal, light_dir), 0.0);
+    // Specular shading
+    Vec3  halfway_dir = normalize(light_dir + view_dir);
+    float spec = pow(max(dot(normal, halfway_dir), 0.0), shininess);
+    // Combine results
+    LightResult result;
+    result.m_diffuse  = light.m_diffuse  * diff;
+    result.m_specular = light.m_specular * spec;
+    //return
+    return result;
+}
