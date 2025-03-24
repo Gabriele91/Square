132e8814c85fd922024dd7b6105e4e1fa03650fe
Gabriele91 <dbgabri@gmail.com>
2025-03-24 22:18:07 +0100

    Add .sqtex format

---
+++ include/Square/Data/ParserTexture.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Square/Config.h"
+#include "Square/Math/Linear.h"
+#include "Square/Resource/Texture.h"
+#include "Square/Data/ParserUtils.h"
+
+namespace Square
+{
+namespace Parser
+{
+	class SQUARE_API Texture
+	{
+	public:
+
+		struct Context
+		{
+			Resource::Texture::Attributes   m_attributes;
+			std::string				        m_url;
+			std::list< ErrorField >	        m_errors;
+			size_t						    m_line{ 0 };
+
+			std::string errors_to_string() const;
+		};
+
+		static bool parse(Context& default_context, const std::string& source);
+
+		static bool parse(Context& default_context, const char*& ptr);
+
+	};
+}
+}
