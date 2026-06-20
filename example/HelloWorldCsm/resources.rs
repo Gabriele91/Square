5a9378546ea6e5f518b8186c70501610673abb3b
Gabriele91 <dbgabri@gmail.com>
2026-06-20 17:55:18 +0200

    Add scene bottle and sphere + update vs launch

--- example/HelloWorldCsm/resources.rs
+++ example/HelloWorldCsm/resources.rs
@@ -3,4 +3,6 @@ resources
 	path "../../common" filter "(\\w)+.rs" //Windows/Linux
 	path "common" filter "(\\w)+.rs" //macOS
 	path "assets"
+	path "assets/sphere_scene"
+	path "assets/bottle_scene"
 }
